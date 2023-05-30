a5b960ba699a92597731bb3c41f712b8608d69bb
User93 <deyvidrospa@hotmail.com>
2023-05-30 18:16:18 -0300

    - Conecção com a API - Validação de login - Requisição e retorno da API.

---
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -0,0 +1,34 @@
+package com.devmasterteam.tasks.service.repository.remote
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RetrofitClient private constructor() {
+
+    companion object {
+        private lateinit var INSTANCE: Retrofit
+
+        private fun getRetronfitInstance(): Retrofit {
+
+            val httpClient = OkHttpClient.Builder()
+
+           if (!:: INSTANCE.isInitialized){
+               synchronized(RetrofitClient::class){
+                   INSTANCE = Retrofit.Builder()
+                       .baseUrl(" http://devmasterteam.com/CursoAndroidAPI/")
+                       .client(httpClient.build())
+                       .addConverterFactory(GsonConverterFactory.create())
+                       .build()
+               }
+           }
+            return INSTANCE
+        }
+
+        fun <T> getService(serviceClas: Class<T>): T {
+            return getRetronfitInstance().create(serviceClas)
+        }
+
+    }
+}
